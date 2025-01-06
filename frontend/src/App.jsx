91aa9c6f434b88166470fbdc54818ed67ec986e0
S-125 <163549520+S-125@users.noreply.github.com>
2025-01-07 00:30:13 +0530

    login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Home from './components/Home';
+import Login from './Login/Login';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import "./App.css"
+import Welcome from './components/Welcome';
+import Register from './Login/Register';
 export default function App() {
   return (
-    <div >
-      <Home/>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Welcome/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
