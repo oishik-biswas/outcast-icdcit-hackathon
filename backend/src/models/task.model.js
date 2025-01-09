be14302634f6be3dfc151c40b236c9481a1a0835
oishik-biswas <biswasoishik5@gmail.com>
2025-01-09 21:38:01 +0530

    Dunno What

--- backend/src/models/task.model.js
+++ backend/src/models/task.model.js
@@ -10,6 +10,10 @@ const taskSchema = new mongoose.Schema({
         required: true,
         trim: true,
     },
+    numberOfHours: {
+        type: Number,
+        default: 0
+    },
     completed: {
         type: Boolean,
         default: false,
